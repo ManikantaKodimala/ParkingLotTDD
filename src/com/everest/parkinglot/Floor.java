40c330757da5ac002ed573e96fee0fc56d7af392
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-11-23 12:45:21 +0530

    Add get number of slots

--- src/com/everest/parkinglot/Floor.java
+++ src/com/everest/parkinglot/Floor.java
@@ -4,21 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Floor {
-    private Map<Integer,Slot> floorMap;
+    private Map<Integer, Slot> floorMap;
     private int floorNumber;
     private int noOfSlots;
 
-    public Floor(int floorNumber, int noOfSlots){
+    public Floor(int floorNumber, int noOfSlots) {
         this.floorNumber = floorNumber;
-        this.noOfSlots=noOfSlots;
-        this.floorMap=new HashMap<Integer,Slot>();
+        this.noOfSlots = noOfSlots;
+        this.floorMap = new HashMap<Integer, Slot>();
         createFloor();
     }
 
     private void createFloor() {
-        for(int i=1;i<noOfSlots;i++) {
+        for (int i = 1; i < noOfSlots; i++) {
             this.floorMap.put(i, new Slot(i));
         }
     }
 
+    public Slot getSlot(int slotNumber) {
+        return this.floorMap.get(slotNumber);
+    }
+
+    public int getNoOfSlots() {
+        return this.noOfSlots;
+    }
+
+    public Slot removeFreeSlot(int slotNumber) {
+        Slot removedSlot = this.floorMap.remove(slotNumber);
+        return removedSlot;
+    }
+
+    public void setFloor(int floorNumber, Slot removedSlot) {
+        this.floorMap.put(floorNumber,removedSlot);
+    }
 }
